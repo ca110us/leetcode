4d2a64a7d9a0fa1811ccd0c952cf7b0f01e83292
ca110us <verycallous@gmail.com>
2021-06-10 14:51:14 +0800

    modify coin change 2

--- coin-change-2.go
+++ coin-change-2.go
@@ -1,7 +1,7 @@
 package main
 
 // 518 https://leetcode-cn.com/problems/coin-change-2/
-// 搞清楚遍历顺序 ！
+// 搞清楚遍历顺序，因为可以无限选取，所以直接正序遍历 ！
 func change(amount int, coins []int) int {
 	dp := make([]int, amount+1)
 	dp[0] = 1

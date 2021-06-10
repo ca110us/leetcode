407fa9d4fd74cd0054672f35ab6395adf9023b1e
ca110us <verycallous@gmail.com>
2021-06-10 10:29:11 +0800

    add coin change 2

---
+++ coin-change-2.go
@@ -0,0 +1,38 @@
+package main
+
+// 518 https://leetcode-cn.com/problems/coin-change-2/
+// 搞清楚遍历顺序 ！
+func change(amount int, coins []int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 1
+
+	for _, coin := range coins {
+		for j := coin; j <= amount; j++ {
+			dp[j] += dp[j-coin]
+		}
+	}
+
+	return dp[amount]
+}
+
+// stupid !!
+// func change(amount int, coins []int) int {
+// 	dp := make([]int, amount+1)
+// 	dp[0] = 1
+
+// 	for _, coin := range coins {
+// 		for j := amount; j > coin-1; j-- {
+// 			k := 1
+// 			for {
+// 				if k*coin > j {
+// 					break
+// 				}
+
+// 				dp[j] += dp[j-(k*coin)]
+// 				k++
+// 			}
+// 		}
+// 	}
+
+// 	return dp[amount]
+// }

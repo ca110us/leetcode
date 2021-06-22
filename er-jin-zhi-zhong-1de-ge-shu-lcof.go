9fb43787adba3627c65177610b8ac6d4c3e70197
edboffical <edboffical@gmail.com>
2021-06-23 00:54:49 +0800

    modify er jin zhi zhong 1de ge shu

--- er-jin-zhi-zhong-1de-ge-shu-lcof.go
+++ er-jin-zhi-zhong-1de-ge-shu-lcof.go
@@ -2,7 +2,7 @@ package main
 
 // 剑指 offer - 15 https://leetcode-cn.com/problems/zi-fu-chuan-de-pai-lie-lcof/
 // 二进制中 1 的个数
-// 位运算 每次消除最右边的 1
+// 位运算 n & (n - 1) 每次消除最右边的 1
 func hammingWeight(num uint32) int {
 	sum := 0
 	for num != 0 {

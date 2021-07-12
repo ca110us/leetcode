485d957bfae502f72f1662ea3896123eb81ecdb6
edboffical <edboffical@gmail.com>
2021-07-12 22:11:06 +0800

    add h-index 2

--- h-index.go
+++ h-index.go
@@ -14,3 +14,24 @@ func hIndex(citations []int) int {
 
 	return ans
 }
+
+// 275 https://leetcode-cn.com/problems/h-index/
+// 第二种方法 二分查找
+func hIndex2(citations []int) int {
+	left, right := 0, len(citations)-1
+	for left < right {
+		mid := (left + right) / 2
+
+		if citations[mid] < len(citations)-mid {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+
+	if citations[left] == 0 {
+		return 0
+	}
+
+	return len(citations) - left
+}

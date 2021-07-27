9ba69e3b89fb6a29f8c1c7015f846341a8505422
edboffical <edboffical@gmail.com>
2021-07-27 15:18:56 +0800

    add 671

---
+++ second-minimum-node-in-a-binary-tree.go
@@ -0,0 +1,48 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 671 https://leetcode-cn.com/problems/second-minimum-node-in-a-binary-tree
+// 深度优先搜索 dfs
+func findSecondMinimumValue(root *TreeNode) int {
+	if root.Left == nil {
+		return -1
+	}
+
+	if root.Left.Val == root.Right.Val && root.Left.Left == nil {
+		return -1
+	}
+
+	sm := -1
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node.Val == root.Val {
+			if node.Left == nil {
+				return
+			}
+
+			dfs(node.Left)
+			dfs(node.Right)
+		} else {
+			if sm == -1 {
+				sm = node.Val
+			} else {
+				sm = min(sm, node.Val)
+			}
+		}
+	}
+
+	dfs(root)
+	return sm
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}

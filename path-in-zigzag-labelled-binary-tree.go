a2bbfef486525083dae5cffed1e3ce41564a1074
edboffical <edboffical@gmail.com>
2021-07-29 21:52:44 +0800

    modify 1104

--- path-in-zigzag-labelled-binary-tree.go
+++ path-in-zigzag-labelled-binary-tree.go
@@ -4,7 +4,7 @@ import "sort"
 
 // 1104 https://leetcode-cn.com/problems/path-in-zigzag-labelled-binary-tree/
 // 用满二叉树的公式 最左边节点 2^(i-1) 最右边节点是 2^i-1
-// 反过来的时候就是下面的函数 2^(i-1) + 2^i-1 - label(从左到右第几个)
+// 反过来的时候就是下面的函数 2^(i-1) + 2^i-1 - label(从左到右排的话是第几个)
 func reverse(label, row int) int {
 	return 1<<(row-1) + 1<<row - 1 - label
 }

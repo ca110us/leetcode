8120504427629271f1bfef48b85f8cbd7bfe1f68
edboffical <edboffical@gmail.com>
2021-08-07 16:34:43 +0800

    modify 457

--- 457-huan-xing-shu-zu-shi-fou-cun-zai-xun-mv87.go
+++ 457-huan-xing-shu-zu-shi-fou-cun-zai-xun-mv87.go
@@ -6,7 +6,7 @@ func circularArrayLoop(nums []int) bool {
 	ln := len(nums)
 	for i := 0; i < ln; i++ {
 		slow, fast := i, next(i, nums)
-		// 判断同号和同向 因为 fast 要做走一步 第二步同向不好预测 所以这里用 next(fast) 和起始点方向去比较
+		// 判断同号和同向 因为 fast 要再走一步 第二步同向不好预测 所以这里用 next(fast) 和起始点方向去比较
 		for nums[fast]*nums[i] > 0 && nums[next(fast, nums)]*nums[i] > 0 {
 			if fast == slow {
 				if slow == next(slow, nums) {

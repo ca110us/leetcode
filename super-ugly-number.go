4bb57a6fa6fc00edb3515f3e62dcd69c654a4f3f
ca110us <verycallous@gmail.com>
2021-08-09 22:04:34 +0800

    modify 313

--- super-ugly-number.go
+++ super-ugly-number.go
@@ -1,6 +1,9 @@
 package main
 
-import "container/heap"
+import (
+	"container/heap"
+	"math"
+)
 
 // 313 https://leetcode-cn.com/problems/super-ugly-number/
 // 最小堆
@@ -48,4 +51,39 @@ func (h *H) Pop() interface{} {
 	v := (*h)[len(*h)-1]
 	*h = (*h)[:len(*h)-1]
 	return v
+}
+
+// 动态规划
+func nthSuperUglyNumber2(n int, primes []int) int {
+	ln := len(primes)
+
+	p := make([]int, ln)
+	nums := make([]int, ln)
+	for i := range p {
+		p[i] = 1
+	}
+	f := make([]int, n+1)
+	f[1] = 1
+
+	for i := 2; i <= n; i++ {
+		min := math.MaxInt32
+		for j := range nums {
+			nums[j] = f[p[j]] * primes[j]
+			min = small(min, nums[j])
+		}
+		f[i] = min
+		for j := range nums {
+			if f[i] == nums[j] {
+				p[j]++
+			}
+		}
+	}
+	return f[n]
+}
+
+func small(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }

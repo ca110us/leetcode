06d8918affb6bef5f25fa4f0817fb8ee7c57faec
edboffical <edboffical@gmail.com>
2021-08-14 22:21:14 +0800

    add 1583

---
+++ count-unhappy-friends.go
@@ -0,0 +1,30 @@
+package main
+
+// 1583 https://leetcode-cn.com/problems/count-unhappy-friends/
+// 模拟
+func unhappyFriends(n int, preferences [][]int, pairs [][]int) (ans int) {
+	order := make([][]int, n)
+	for i, preference := range preferences {
+		order[i] = make([]int, n)
+		for j, p := range preference {
+			order[i][p] = j
+		}
+	}
+	match := make([]int, n)
+	for _, p := range pairs {
+		match[p[0]] = p[1]
+		match[p[1]] = p[0]
+	}
+
+	for x, y := range match {
+		index := order[x][y]
+		for _, u := range preferences[x][:index] {
+			v := match[u]
+			if order[u][x] < order[u][v] {
+				ans++
+				break
+			}
+		}
+	}
+	return
+}
